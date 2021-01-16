d911e8d35db1a3b351a5f5ce04ef71acb0790540
hasan basri <samli16@itu.edu.tr>
2021-01-16 22:58:10 +0300

    Delete server_v3.py

--- server_v3.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, redirect, request, url_for
-from flask_login import login_manager
-from werkzeug.security import check_password_hash, generate_password_hash
